6e0a75a9f72a09f45cc9150c4af94e3b0468e173
Anik Chakrabortty <anik.chakrabortty@sourcetop.com>
2020-12-13 21:06:17 +0600

    district code added

--- index.php
+++ index.php
@@ -1,16 +1,53 @@
 <?php
-
+use BDLocation\BD;
 use MoneyInWords\MoneyInWords;
 
 require './vendor/autoload.php';
 
-$converter = new MoneyInWords; 
+$converter = new MoneyInWords(); 
+
+$divisions = BD::division()->all(); 
 
 
-$number = 3125639562;
+if(isset($_POST['division_name'])){
+    $query = $_POST['division_name']; 
+    
+    $result = BD::district()->all();
 
-$kothay = ($converter->moneyToWords($number));
+    foreach($result as $district){
 
-print_r($kothay);  
+        $d_name = strtolower($district->name);
+        //var_dump($d_name); 
+        $query = strtolower($query);
+
+        if(strpos($d_name, $query) !== FALSE){
+            echo $d_name; 
+            echo '<br/>'; 
+        }
+    }        
+}
+
+if(isset($_POST['number'])){
+    $number = $_POST['number']; 
+    $word = $converter->numberToWords($number);
+    print_r($word[0]); 
+}
 
 
+
+
+?> 
+
+Divison Search:
+<form action = "" method = "POST"> 
+<input name="division_name" type="text" value=""> 
+<input type="submit">
+</form> 
+
+<br><br> 
+
+Number to Words in Money: 
+<form action = "" method = "POST"> 
+<input name="number" type="text" value=""> 
+<input type="submit">
+</form> 
